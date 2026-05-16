f7bd5b7432f90c3e76ad9fe62818673afd8bcf5f
mauryadeepakprettify <mauryadeepak.prettify@gmail.com>
2026-05-16 15:23:52 +0530

    style: enquire modal

--- src/components/organisms/EnquireModal.jsx
+++ src/components/organisms/EnquireModal.jsx
@@ -8,7 +8,7 @@ const EnquireModal = () => {
 
   return (
     <div
-      className={`model enquire-modal ${isModal === "enquire" ? "is-open" : ""}`}
+      className={`model  enquire-modal ${isModal === "enquire" ? "is-open" : ""}`}
     >
       <button className="close" onClick={() => closeModal()}>
         <Image
@@ -18,41 +18,49 @@ const EnquireModal = () => {
           alt="close"
         />
       </button>
-      <div className="modal-body">
-        <h2>Find Your Perfect Getaway</h2>
-        <p>
-          Tell us a little about yourself and receive curated villa experiences
-          and exclusive offers.
-        </p>
+      <div className="modal-body vertical-scroll">
+        <div className="content">
+          <h2>Find Your Perfect Getaway</h2>
+          <p>
+            Tell us a little about yourself and receive curated villa
+            experiences and exclusive offers.
+          </p>
 
-        <form action="">
-          <label htmlFor="">
-            <span>
-              First Name <sup>*</sup>
-            </span>
-            <input type="text" placeholder="Shubh" />
-          </label>
-          <label htmlFor="">
-            <span>
-              Last Name <sup>*</sup>
-            </span>
-            <input type="text" placeholder="Somani" />
-          </label>
-          <label htmlFor="">
-            <span>
-              City <sup>*</sup>
-            </span>
-            <input type="text" placeholder="Goa" />
-          </label>
-          <label htmlFor="">
-            <span>
-              Enter your query here <sup>*</sup>
-            </span>
-            <textarea rows="4" placeholder="Your text here..." />
-          </label>
+          <form action="">
+            <label htmlFor="">
+              <span>
+                First Name <sup>*</sup>
+              </span>
+              <input type="text" placeholder="Shubh" />
+            </label>
+            <label htmlFor="">
+              <span>
+                Last Name <sup>*</sup>
+              </span>
+              <input type="text" placeholder="Somani" />
+            </label>
+            <label htmlFor="">
+              <span>
+                Phone Number <sup>*</sup>
+              </span>
+              <input type="phone" placeholder="1234567890" />
+            </label>
+            <label htmlFor="">
+              <span>
+                City <sup>*</sup>
+              </span>
+              <input type="text" placeholder="Goa" />
+            </label>
+            <label htmlFor="">
+              <span>
+                Enter your query here <sup>*</sup>
+              </span>
+              <textarea rows="4" placeholder="Your text here..." />
+            </label>
 
-          <button className="btn">Submit</button>
-        </form>
+            <button className="btn">Submit</button>
+          </form>
+        </div>
       </div>
     </div>
   );
