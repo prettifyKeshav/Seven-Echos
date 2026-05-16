b6cdeddf41b416a295550683ba514af9e25b2eb1
mauryadeepakprettify <mauryadeepak.prettify@gmail.com>
2026-05-16 15:09:07 +0530

    modal: enquire

--- src/components/organisms/EnquireModal.jsx
+++ src/components/organisms/EnquireModal.jsx
@@ -1,10 +1,61 @@
-
+"use client";
+import { useModal } from "@/hooks/useModal";
+import Image from "next/image";
+import { useSelector } from "react-redux";
 const EnquireModal = () => {
+  const { openModal, closeModal } = useModal();
+  const { isModal } = useSelector((state) => state.modal);
+
   return (
-    <div className="">
+    <div
+      className={`model enquire-modal ${isModal === "enquire" ? "is-open" : ""}`}
+    >
+      <button className="close" onClick={() => closeModal()}>
+        <Image
+          src="/assets/icon/close.svg"
+          width={24}
+          height={24}
+          alt="close"
+        />
+      </button>
+      <div className="modal-body">
+        <h2>Find Your Perfect Getaway</h2>
+        <p>
+          Tell us a little about yourself and receive curated villa experiences
+          and exclusive offers.
+        </p>
 
+        <form action="">
+          <label htmlFor="">
+            <span>
+              First Name <sup>*</sup>
+            </span>
+            <input type="text" placeholder="Shubh" />
+          </label>
+          <label htmlFor="">
+            <span>
+              Last Name <sup>*</sup>
+            </span>
+            <input type="text" placeholder="Somani" />
+          </label>
+          <label htmlFor="">
+            <span>
+              City <sup>*</sup>
+            </span>
+            <input type="text" placeholder="Goa" />
+          </label>
+          <label htmlFor="">
+            <span>
+              Enter your query here <sup>*</sup>
+            </span>
+            <textarea rows="4" placeholder="Your text here..." />
+          </label>
+
+          <button className="btn">Submit</button>
+        </form>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default EnquireModal
+export default EnquireModal;
